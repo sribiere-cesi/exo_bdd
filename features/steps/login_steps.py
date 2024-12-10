decb44ee0ccf072683530bfff70e4f6c147eca5d
Sebastien RIBIERE <sebastien.ribiere.ext@jellysmack.com>
2024-12-10 21:13:28 +0100

    fix: exercice

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -5,12 +5,4 @@ def step_impl(context, email, password):
     context.email = email
     context.password = password
 
-@when('il saisit "{email}" et "{password}"')
-def step_impl(context, email, password):
-    context.input_email = email
-    context.input_password = password
 
-@then('il est redirigé vers le tableau de bord')
-def step_impl(context):
-    assert context.input_email == context.email
-    assert context.input_password == context.password
